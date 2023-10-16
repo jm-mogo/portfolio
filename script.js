0fe6a9f1583671a1415365e9d0323b04943a2ea2
mike <josemimore0@gmail.com>
2023-10-16 18:31:03 -0400

    new style for the hamburger menu, with transitions when the menu is open

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-let openHam = document.querySelector("#openHam");
-let closeHam = document.querySelector("#closeHam");
-let navigationItems = document.querySelector("#navigation-items");
+const checkbox = document.querySelector("input");
+const navigationItems = document.querySelector(".navigation-items");
 
-const hamburgerEvent = (navigation, close, open) => {
-    navigationItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
-};
-
-openHam.addEventListener("click", () =>
-    hamburgerEvent("flex", "block", "none")
-);
-closeHam.addEventListener("click", () =>
-    hamburgerEvent("none", "none", "block")
-);
+checkbox.addEventListener("click", () => {
+    if (checkbox.checked) {
+        navigationItems.style = "transform: none";
+    } else {
+        navigationItems.style = "transform: translate(-100%, 0);";
+    }
+});
