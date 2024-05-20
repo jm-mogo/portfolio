87f51d1a65ae9852c3315b0481667ff7b0fe2c15
mike <josemimore0@gmail.com>
2024-05-20 19:25:21 -0400

    projects links redirects to projects section in the page, and the mobile version closes menu

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ checkbox.addEventListener("click", () => {
         navigationItems.style = "transform: translate(-100%, 0);";
     }
 });
+
+const mediaQuery = window.matchMedia("(max-width:768px)");
+
+navigationItems.addEventListener("click", () => {
+    if (mediaQuery.matches) {
+        navigationItems.style = "transform: translate(-100%, 0)";
+        checkbox.checked = false;
+    }
+});
+
+function changeCss() {
+    var navElement = document.querySelector("header");
+    this.scrollY < 1
+        ? (navElement.style.backgroundColor = "")
+        : (navElement.style.backgroundColor = "#1d1727");
+}
+
+window.addEventListener("scroll", changeCss, false);
